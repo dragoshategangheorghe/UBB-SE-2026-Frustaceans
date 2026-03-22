0337b4588f644bc5cc890dc16ca7988990dc782a
Kovács Levente <levente.kovacs4@stud.ubbcluj.ro>
2026-03-22 20:48:29 +0200

    added repository interfaces

---
+++ PharmacyApp/Repositories/IItemRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PharmacyApp.Models;
+
+namespace PharmacyApp.Repositories
+{
+    internal interface IItemRepository
+    {
+        bool addItem(Item newItem);
+        bool removeItem(string idToBeRemoved);
+        Item getItem(string id);
+        List<Item> getItemsByName(string name);
+        bool changeItemInfo(string id, Item newItem);
+        bool checkItemExists(string id);
+    }
+}
---
+++ PharmacyApp/Repositories/ISubstanceRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PharmacyApp.Models;
+
+namespace PharmacyApp.Repositories
+{
+    internal interface ISubstanceRepository
+    {
+        bool addSubstances(Substance newSubstance);
+        bool removeSubstance(string name);
+        Substance getSubstance(string name);
+        bool changeSubstanceInfo(string name, Substance newSubstance);
+        bool checkSubstanceExists(string name);
+    }
+}
