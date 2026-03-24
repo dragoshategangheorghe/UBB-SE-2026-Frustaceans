5054b929eafa036046047934163410ce5d38b823
lupeanc <clupean1365@gmail.com>
2026-03-24 13:55:30 +0200

    outline for notifications and admin service

---
+++ PharmacyApp/Repositories/ItemRepository.cs
@@ -0,0 +1,43 @@
+﻿using PharmacyApp.Interfaces;
+using PharmacyApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PharmacyApp.Repositories
+{
+    internal class ItemRepository : IItemRepository
+    {
+        public bool addItem(Item newItem)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool changeItemInfo(string id, Item newItem)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool checkItemExists(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Item getItem(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Item> getItemsByName(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool removeItem(string idToBeRemoved)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- PharmacyApp/Services/AdminService.cs
+++ PharmacyApp/Services/AdminService.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using PharmacyApp.Interfaces;
+using PharmacyApp.Models;
+using PharmacyApp.Repositories;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +11,66 @@ namespace PharmacyApp.Services
 {
     internal class AdminService
     {
+        ItemRepository itemRepository;
+        SubstanceRepository substanceRepository;
+        public AdminService(ItemRepository itemRepo, SubstanceRepository substanceRepo)
+        {
+            this.itemRepository = itemRepo;
+            this.substanceRepository = substanceRepo;
+        }
+
+        public void addItem(Item newItem)
+        {
+            try
+            {
+                validateItemAdd(newItem);
+                itemRepository.addItem(newItem);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error adding item: {ex.Message}");
+                //return;
+
+            }
+        }
+
+        public void removeItem(string id)
+        {
+            itemRepository.removeItem(id);
+        }
+
+       public void updateItem(string id, Item item)
+        {
+            itemRepository.changeItemInfo(id, item);
+        }
+
+        public void addSubstance(Substance newSubstance)
+        {
+            substanceRepository.addSubstances(newSubstance);
+        }
+
+        public void removeSubstance(Substance substance)
+        {
+            substanceRepository.removeSubstance(substance.Name);
+        }
+
+        public void updateSubstance(string name, Substance substance)
+        {
+            substanceRepository.changeSubstanceInfo(name, substance);
+        }
+
+        public void validateItemAdd(Item item)
+        {
+            if (item.Name == "" ||
+                item.Producer == "" ||
+                item.Price <= 0 ||
+                item.NumberOfPills <= 0 ||
+                item.Quantity < 0 ||
+                item.DiscountPercentage < 0 ||
+                item.ActiveSubstances.Count == 0)
+            {
+                throw new Exception("Invalid item data. Please check the input and try again.");
+            }
+        }
     }
 }
