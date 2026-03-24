5080ba6188adf045c75550a2cefeaaa3f772e2dc
dragoshategangheorghe <dragoshategan3@gmail.com>
2026-03-24 17:49:09 +0200

    Moved validation to seperate class

--- PharmacyApp/Features/Accounts/Logic/LoginService.cs
+++ PharmacyApp/Features/Accounts/Logic/LoginService.cs
@@ -18,48 +18,11 @@ namespace PharmacyApp.Features.Accounts.Logic
             this.users = users;
         }
 
-        public bool isCorrectEmailFormat(string email)
-        {
-            if (string.IsNullOrWhiteSpace(email)) return false;
-            email = email.Trim();
-
-            //pattern: <text>@<text>.<text>
-            string pattern = @"^.+@.+\..+";
-            return Regex.IsMatch(email, pattern);
-        }
-        public bool isCorrectPasswordFormat(string password)
-        {
-            if (string.IsNullOrWhiteSpace(password)) return false;
-
-            //password should have:
-            //-min 8 chars, at least one capital letter, at least one small letter,at least one digit, at least one of {!,@,#,%,^,*}, no other char types   
-            string pattern = @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#%^*])[A-Za-z\d!@#%^*]{8,}$";
-            return Regex.IsMatch(password,pattern);
-        }
-
-        public bool isCorrectPhoneNumberFormat(string phoneNumber)
-        {
-            if (string.IsNullOrWhiteSpace(phoneNumber)) return false;
-            phoneNumber = phoneNumber.Trim();
-
-            string pattern = @"^[0-9]+$";
-            return Regex.IsMatch(phoneNumber,pattern);
-        }
-
-        public bool isCorrectUsernameFormat(string username)
-        {
-            if (string.IsNullOrWhiteSpace(username))
-                return false;
-            username = username.Trim();
-
-            string pattern = @"^[A-Za-z_]+$";
-            return Regex.IsMatch(username,pattern);
-        }
 
 
         public User Login(string email,string password)
         {
-            if (!isCorrectEmailFormat(email))
+            if (!UserValidationService.isCorrectEmailFormat(email))
                 throw new Exception("Not a valid e-mail");
             try
             {
@@ -77,9 +40,9 @@ namespace PharmacyApp.Features.Accounts.Logic
 
         public void Register(string email, string password, string confirmPassword, string username = "", string phoneNumber = "")
         {
-            if (!isCorrectEmailFormat(email))
+            if (!UserValidationService.isCorrectEmailFormat(email))
                 throw new Exception("Not a valid email");
-            if (!isCorrectPasswordFormat(password))
+            if (!UserValidationService.isCorrectPasswordFormat(password))
                 throw new Exception("Incorrect format");
             if (password != confirmPassword)
                 throw new Exception("Passwords don't match");
@@ -90,6 +53,8 @@ namespace PharmacyApp.Features.Accounts.Logic
                 throw new Exception("Email already linked to an account");
             }
             catch (UserNotFoundException) { }
+            
+            
             var passwordHash = SecurityService.HashPassword(password);
             var discountNotificationsSetting = false;
             users.Add(email,phoneNumber,passwordHash,username,discountNotificationsSetting);
---
+++ PharmacyApp/Features/Accounts/Logic/UserValidationService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace PharmacyApp.Features.Accounts.Logic
+{
+    public class UserValidationService
+    {
+        public static bool isCorrectEmailFormat(string email)
+        {
+            if (string.IsNullOrWhiteSpace(email)) return false;
+            email = email.Trim();
+
+            //pattern: <text>@<text>.<text>
+            string pattern = @"^.+@.+\..+";
+            return Regex.IsMatch(email, pattern);
+        }
+        public static bool isCorrectPasswordFormat(string password)
+        {
+            if (string.IsNullOrWhiteSpace(password)) return false;
+
+            //password should have:
+            //-min 8 chars, at least one capital letter, at least one small letter,at least one digit, at least one of {!,@,#,%,^,*}, no other char types   
+            string pattern = @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#%^*])[A-Za-z\d!@#%^*]{8,}$";
+            return Regex.IsMatch(password, pattern);
+        }
+
+        public static bool isCorrectPhoneNumberFormat(string phoneNumber)
+        {
+            if (string.IsNullOrWhiteSpace(phoneNumber)) return false;
+            phoneNumber = phoneNumber.Trim();
+
+            string pattern = @"^[0-9]+$";
+            return Regex.IsMatch(phoneNumber, pattern);
+        }
+
+        public static bool isCorrectUsernameFormat(string username)
+        {
+            if (string.IsNullOrWhiteSpace(username))
+                return false;
+            username = username.Trim();
+
+            string pattern = @"^[A-Za-z_]+$";
+            return Regex.IsMatch(username, pattern);
+        }
+
+    }
+}
