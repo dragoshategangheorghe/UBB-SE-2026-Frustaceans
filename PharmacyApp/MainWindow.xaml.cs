4be32d283ec6b6588edc57af3e518849dd9e6461
Kovács Levente <levente.kovacs4@stud.ubbcluj.ro>
2026-03-25 23:09:23 +0200

    deleted old testing code for users, items and orders

--- PharmacyApp/MainWindow.xaml.cs
+++ PharmacyApp/MainWindow.xaml.cs
@@ -27,38 +27,6 @@ namespace PharmacyApp
     {
         public MainWindow()
         {
-            /* TEST CODE!!!
-            IUsersRepository usersRepository = new SQLUsersRepository();
-
-            System.Diagnostics.Debug.WriteLine(SQLUtility.GetConnectionString());
-            System.Diagnostics.Debug.WriteLine(usersRepository.GetUserById(1).Email);
-            System.Diagnostics.Debug.WriteLine(usersRepository.UserExists(1));
-            System.Diagnostics.Debug.WriteLine(usersRepository.UserExists("dgfs@ghsg.com"));
-            System.Diagnostics.Debug.WriteLine(usersRepository.UserExists("dgfgfdssfds@ghsg.com"));
-            System.Diagnostics.Debug.WriteLine(usersRepository.UserExists(2));
-
-            User user1 = usersRepository.GetUserById(1);
-            List<User> users = usersRepository.GetAllUsers();
-
-
-            User newUser = user1;
-
-            newUser.Email = "newmail@gdsdd.com";
-            newUser.PhoneNumber = "0727063545";
-            newUser.PasswordHash = "newpass";
-;           //newUser.RemovePeriodNote(2);
-            //newUser.RemoveUserDiscount(1); // remove the 30% discount
-            newUser.CycleDays = 25;
-            newUser.PMSOption = 4;
-            newUser.StartPeriodDate = new DateOnly(2026, 3, 25);
-            //newUser.RemoveStockAlert(1);
-            //newUser.RemoveFavoriteItem(2);
-
-            usersRepository.UpdateUser(newUser);
-            */
-
-
-
             InitializeComponent();
         }
     }
