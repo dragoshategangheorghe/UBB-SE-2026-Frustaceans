5166bfa382b00b164db203098d8748b0c2df2c56
lupeanc <clupean1365@gmail.com>
2026-03-26 18:41:04 +0200

    -

--- PharmacyApp/MainWindow.xaml.cs
+++ PharmacyApp/MainWindow.xaml.cs
@@ -1,11 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
 using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
-using PharmacyApp.Common.Repositories;
-using PharmacyApp.Models;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using PharmacyApp.Common.Repositories;
+using PharmacyApp.Models;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -19,28 +27,38 @@ namespace PharmacyApp
     {
         public MainWindow()
         {
-            /*
-            ISubstancesRepository substancesRepo = new SQLSubstancesRepository();
-            System.Diagnostics.Debug.WriteLine(substancesRepo.SubstanceExists("ibuprophen"));
-            System.Diagnostics.Debug.WriteLine(substancesRepo.SubstanceExists("dggfddg"));
-            Substance a = substancesRepo.GetSubstance("ibuprophen");
-            try
-            {
-                Substance b = substancesRepo.GetSubstance("nooooooo");
-            }
-            catch (Exception ex) {
-                System.Diagnostics.Debug.WriteLine(ex.Message);
-            }
-            substancesRepo.RemoveSubstance("meth");
-            try
-            {
-                substancesRepo.RemoveSubstance("nooooooo");
-            }
-            catch (Exception ex)
-            {
-                System.Diagnostics.Debug.WriteLine(ex.Message);
-            }
+            /* TEST CODE!!!
+            IUsersRepository usersRepository = new SQLUsersRepository();
+
+            System.Diagnostics.Debug.WriteLine(SQLUtility.GetConnectionString());
+            System.Diagnostics.Debug.WriteLine(usersRepository.GetUserById(1).Email);
+            System.Diagnostics.Debug.WriteLine(usersRepository.UserExists(1));
+            System.Diagnostics.Debug.WriteLine(usersRepository.UserExists("dgfs@ghsg.com"));
+            System.Diagnostics.Debug.WriteLine(usersRepository.UserExists("dgfgfdssfds@ghsg.com"));
+            System.Diagnostics.Debug.WriteLine(usersRepository.UserExists(2));
+
+            User user1 = usersRepository.GetUserById(1);
+            List<User> users = usersRepository.GetAllUsers();
+
+
+            User newUser = user1;
+
+            newUser.Email = "newmail@gdsdd.com";
+            newUser.PhoneNumber = "0727063545";
+            newUser.PasswordHash = "newpass";
+;           //newUser.RemovePeriodNote(2);
+            //newUser.RemoveUserDiscount(1); // remove the 30% discount
+            newUser.CycleDays = 25;
+            newUser.PMSOption = 4;
+            newUser.StartPeriodDate = new DateOnly(2026, 3, 25);
+            //newUser.RemoveStockAlert(1);
+            //newUser.RemoveFavoriteItem(2);
+
+            usersRepository.UpdateUser(newUser);
             */
+
+
+
             InitializeComponent();
             MainFrame.Navigate(typeof(Features.Products_Catalogue.HomePage));
         }
@@ -73,7 +91,6 @@ namespace PharmacyApp
         private void OnAdminClicked(object sender, RoutedEventArgs e)
         {
             MainFrame.Navigate(typeof(Features.Pharmacy_Management.EditPage));
-
         }
     }
 }
