cecedf1d87735547e3f4e352bb3db888fcdf0aa3
Allex3 <103828759+Allex3@users.noreply.github.com>
2026-03-27 03:38:03 +0200

    Added custom calendar views for Period Tracker, no functionality.

--- PharmacyApp/Features/Period_Tracker/Views/PeriodTrackerPage.xaml.cs
+++ PharmacyApp/Features/Period_Tracker/Views/PeriodTrackerPage.xaml.cs
@@ -1,3 +1,4 @@
+using Microsoft.UI;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
@@ -5,13 +6,9 @@ using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+using PharmacyApp.Features.Period_Tracker.ViewModels;
+using Syncfusion.UI.Xaml.Calendar;
+
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -23,9 +20,15 @@ namespace PharmacyApp.Features.Period_Tracker.Views
     /// </summary>
     public sealed partial class PeriodTrackerPage : Page
     {
+        public PeriodTrackerViewModel ViewModel { get; set; } = new PeriodTrackerViewModel();
         public PeriodTrackerPage()
         {
             InitializeComponent();
+            //CalendarsStackPanel.Visibility=Visibility.Collapsed;
+        }
+        private void UpdateCalendar()
+        {
+            
         }
     }
 }
